eaf5efbbda1b7611a45fcb73aec3909f2ba63b63
Peixky <20050322lp@gmail.com>
2026-01-15 18:04:19 +0800

    Refactor array handling and input/output logic

--- itsa11.c
+++ itsa11.c
@@ -1,19 +1,20 @@
+#include <stdio.h>
+
 int main() {
-   int col, row;
-   while(scanf("%d %d", &row, &col) != EOF){
-      int array[row][col];
-      for(int i = 0; i < row; i++){
-         for(int j = 0; j < col; j++){
-            scanf("%d", &array[i][j]);
+   int row, col;
+   while (scanf("%d %d", &row, &col) != EOF) {
+      int a[100][100];
+      for (int i = 0; i < row; i++) {
+         for (int j = 0; j < col; j++) {
+               scanf("%d", &a[i][j]);
          }
       }
-      for(int i = 0; i < col; i++){
-         for(int j = 0; j < row; j++){
-            printf("%d ", array[j][i]);
+      for (int j = 0; j < col; j++) {
+         for (int i = 0; i < row; i++) {
+               if (i != 0) printf(" ");
+               printf("%d", a[i][j]);
          }
          printf("\n");
       }
    }
-   
-   
 }

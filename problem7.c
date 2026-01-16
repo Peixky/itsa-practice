5fd91aa66b6cbbeea3e76ec2d666c5e144691b5f
Peixky <20050322lp@gmail.com>
2026-01-16 12:55:28 +0800

    Create problem7.c

---
+++ problem7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int n;
+   scanf("%d", &n);
+   int x, max;
+   scanf("%d", &max); 
+   for (int i = 1; i < n; i++) {
+      scanf("%d", &x);
+      if (x > max) {
+         max = x;
+      }
+   }
+
+   printf("%d\n", max);
+}
